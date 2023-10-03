5d2024b63070d058d7c84a75fdd6cf14bb6183c8
munyua-arch <dmurimi919@gmail.com>
2023-10-03 10:55:19 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(c, chr(c - 33)), end="")
+a = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - a)), end="")
+    a = 32 if a == 0 else 0
